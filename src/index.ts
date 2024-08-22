52f8b87c3b13f172d04c8e42d7edd5472c3f0b58
泉户泽宇 <1624910218@qq.com>
2024-08-22 13:02:54 +0800

    ADD: 测试功能

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,9 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.on('message', (session) => {
+    if (session.content === '114514') {
+      session.send('1919810')
+    }
+  })
 }
