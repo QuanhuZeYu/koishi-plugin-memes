5fada35dd895e4727beb5c6a438569adcc78a1ea
泉户泽雨 <1624910218@qq.com>
2024-09-20 22:37:24 +0800

    优化clown命令处理动态图像判断逻辑
    在clown.tsx文件中，优化了对输入图像是否为GIF的判断逻辑。现在，通过异步操作获取memelib实例，并使用改进的工具函数isGif来确定输入图像类型。如果图像是GIF，则动态创建一个引用（quote）元素；如果不是，则不添加任何额外元素。这一改动提升了图像处理逻辑的清晰度和维护性。

--- src/commands/clown.tsx
+++ src/commands/clown.tsx
@@ -20,8 +20,9 @@ async function clown(argv: Argv, message: string) {
         const input = imageArgs[0]
         const clown = await getMemelib().memelib.clown(input)
         const image = await getMemelib().memelib.tools.imageTools.isGif(clown) ? <img src={"data:image/gif;base64," + clown.toString("base64")} /> : <img src={"data:image/png;base64," + clown.toString("base64")} />
+        const isQuote = await getMemelib().memelib.tools.imageTools.isGif(clown) ? <quote id={argv.session.messageId} /> : ""
         const messageStruct = <message>
-            <quote id={argv.session.messageId} />
+            {isQuote}
             {image}
         </message>;
         return await argv.session.send(messageStruct)
